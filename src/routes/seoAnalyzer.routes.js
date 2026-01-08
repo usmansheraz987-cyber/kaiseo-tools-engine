e97ce29c7cd7b886a3c96e1363908bf684275522
Mueed Basit <usmansheraz789@gmail.com>
2026-01-08 22:51:08 +0500

    dgfddhgfh

--- src/routes/seoAnalyzer.routes.js
+++ src/routes/seoAnalyzer.routes.js
@@ -3,7 +3,6 @@ import { seoAnalyzerController } from "../tools/seoAnalyzer/v1/controller.js";
 
 const router = express.Router();
 
-// SEO Analyzer v1
 router.post("/api/v1/seo-analyzer", seoAnalyzerController);
 
 export default router;
