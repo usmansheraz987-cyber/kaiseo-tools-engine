cf769bc4fe8bc8a24de4d1d24b4e9fad9497d9c2
Mueed Basit <usmansheraz789@gmail.com>
2026-01-10 23:21:14 +0500

    fsdsd

--- src/routes/seoAnalyzer.routes.js
+++ src/routes/seoAnalyzer.routes.js
@@ -3,27 +3,34 @@ import express from "express";
 // v1
 import { seoAnalyzerController } from "../tools/seoAnalyzer/v1/controller.js";
 
-// v2
+// v2 analyze
 import { seoAnalyzerV2Controller } from "../tools/seoAnalyzer/v2/controller.js";
-//comparision
-import { seoAnalyzerV2CompareController } from "../tools/seoAnalyzer/v2/comparison/controller.js";
-router.post(
-  "/seo-analyzer/v2/compare",
-  seoAnalyzerV2CompareController
-);
 
+// v2 compare
+import { seoAnalyzerV2CompareController } from "../tools/seoAnalyzer/v2/comparison/controller.js";
 
 const router = express.Router();
 
-// health check
+/* --------------------
+   Health check
+-------------------- */
 router.get("/_alive", (req, res) => {
   res.send("SEO ANALYZER ROUTE IS ALIVE");
 });
 
-// v1 (stable)
+/* --------------------
+   v1
+-------------------- */
 router.post("/seo-analyzer/v1/analyze", seoAnalyzerController);
 
-// v2 (advanced)
+/* --------------------
+   v2 analyze
+-------------------- */
 router.post("/seo-analyzer/v2/analyze", seoAnalyzerV2Controller);
+
+/* --------------------
+   v2 compare
+-------------------- */
+router.post("/seo-analyzer/v2/compare", seoAnalyzerV2CompareController);
 
 export default router;
