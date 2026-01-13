cba3557ba68aa527a4630dbfc0a8dad76c83472f
Mueed Basit <usmansheraz789@gmail.com>
2026-01-13 19:37:43 +0500

    fdsfsdfs

--- src/tools/seoAnalyzer/v4/controller.js
+++ src/tools/seoAnalyzer/v4/controller.js
@@ -1,8 +1,29 @@
-export async function seoAnalyzerV4Controller(req, res) {
-  return res.json({
-    success: true,
-    version: "v4",
-    test: "controller alive"
-  });
-}
+import { runSeoAnalyzerV3 } from "../v3/service.js";
 
+export async function seoAnalyzerV4Controller(req, res, next) {
+  try {
+    const { url, primaryQuery } = req.body;
+
+    if (!url || !primaryQuery) {
+      return res.status(400).json({
+        success: false,
+        error: "URL_AND_PRIMARY_QUERY_REQUIRED"
+      });
+    }
+
+    const v3Result = await runSeoAnalyzerV3({
+      url,
+      primaryQuery
+    });
+
+    return res.json({
+      success: true,
+      version: "v4",
+      debug: {
+        v3Keys: Object.keys(v3Result || {})
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+}
