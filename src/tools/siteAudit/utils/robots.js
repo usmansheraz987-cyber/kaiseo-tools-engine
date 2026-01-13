27c884bd39236aaa62228af0477db486dd76c6a2
Mueed Basit <usmansheraz789@gmail.com>
2026-01-13 22:51:41 +0500

    fix robots fetchPage import path

--- src/tools/siteAudit/utils/robots.js
+++ src/tools/siteAudit/utils/robots.js
@@ -1,13 +1,22 @@
 import robotsParser from "robots-parser";
-import { fetchPage } from "../../lib/fetcher/fetchPage.js";
+import { fetchPage } from "../../../lib/fetcher/fetchPage.js";
 
+/**
+ * Loads and parses robots.txt for a given base URL.
+ * Always fails safely.
+ */
 export async function loadRobotsRules(baseUrl) {
   try {
     const robotsUrl = new URL("/robots.txt", baseUrl).toString();
+
     const res = await fetchPage(robotsUrl);
 
-    return robotsParser(robotsUrl, res.body || "");
-  } catch {
+    return robotsParser(
+      robotsUrl,
+      res?.body || ""
+    );
+  } catch (err) {
+    // Fail-safe: allow crawling if robots.txt cannot be fetched
     return robotsParser("", "");
   }
 }
