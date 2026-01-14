6bd1868656a2eb44b9fff7d5f63fc2bfb0b8201d
Mueed Basit <usmansheraz789@gmail.com>
2026-01-14 20:35:25 +0500

    sdfffd

--- src/tools/siteAudit/service.js
+++ src/tools/siteAudit/service.js
@@ -3,7 +3,8 @@ import { loadRobotsRules } from "./utils/robots.js";
 import { createCrawlQueue } from "./crawl/queue.js";
 import { runChecksOnPage } from "./checks/runChecks.js";
 import { buildReport } from "./report/summarize.js";
-import fetchPage from "../../lib/fetcher/fetchPage.js";
+import { fetchPageHtml } from "../../lib/fetcher/fetchPage.js";
+
 
 
 export async function runSiteAudit(input) {
@@ -22,7 +23,8 @@ export async function runSiteAudit(input) {
   let rootPage = null;
 
   try {
-    const rootFetch = await fetchPage(baseUrl);
+    const rootFetch = await fetchPageHtml(baseUrl);
+
 
 
     rootPage = {
