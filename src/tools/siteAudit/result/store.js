9dceed1de5495d6c89220f41361270199e24c402
Mueed Basit <usmansheraz789@gmail.com>
2026-01-17 22:50:10 +0500

    fasff

--- src/tools/siteAudit/result/store.js
+++
@@ -1,9 +0,0 @@
-const resultStore = new Map();
-
-export function saveResult(auditId, data) {
-  resultStore.set(auditId, data);
-}
-
-export function getResult(auditId) {
-  return resultStore.get(auditId) || null;
-}
